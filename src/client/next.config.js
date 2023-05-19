b57794c20f53cde93c5b8f5714550aaf35fcebeb
DJDavid98 <djdavid98@protonmail.com>
2023-05-19 03:58:07 +0200

    Add /discord redirect via Nestjs

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -2,17 +2,8 @@
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config({ path: path.join(__dirname, '..', '..', '.env') });
-
 module.exports = {
   distDir: '../../.next',
-  redirects: async () => [
-    {
-      source: '/discord',
-      destination: process.env.DISCORD_INVITE_URL,
-      permanent: false,
-    },
-  ],
   experimental: {
     appDir: true,
   },
--- src/server/view/view.controller.ts
+++ src/server/view/view.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 
 import { ViewService } from './view.service';
+import { serverEnv } from '../server-env';
 
 @Controller()
 export class ViewController {
   constructor(private viewService: ViewService) {}
 
-  @Get()
+  @Get('/discord')
+  @Redirect(serverEnv.DISCORD_INVITE_URL, HttpStatus.TEMPORARY_REDIRECT)
+  public discordInvite() {
+    // noop
+  }
+
+  @Get('*')
   public async showIndex(@Req() req: Request, @Res() res: Response) {
     await this.viewService.handler(req, res);
   }
