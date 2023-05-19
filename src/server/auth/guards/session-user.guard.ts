5f7af4d0b920cbdb712d713a219f967f2e738eee
DJDavid98 <djdavid98@protonmail.com>
2023-05-19 03:58:07 +0200

    Add debug logging for session guard

--- src/server/auth/guards/session-user.guard.ts
+++ src/server/auth/guards/session-user.guard.ts
@@ -1,28 +1,34 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { UsersService } from '../../users/users.service';
-import { DiscordUsersService } from '../../discord-users/discord-users.service';
 
 @Injectable()
 export class SessionUserGuard implements CanActivate {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly discordUsersService: DiscordUsersService,
-  ) {}
+  logger = new Logger(SessionUserGuard.name);
+
+  constructor(private readonly usersService: UsersService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const { userId } = request.session;
     if (!userId) {
+      this.logger.debug('Session did not contain userId for request, denying');
       return false;
     }
 
     // Retrieve user information from the users service
     const user = await this.usersService.findOne(userId);
     if (!user) {
+      this.logger.debug('User not found for userId in session, denying');
       return false;
     }
 
     // Store the user object in the request for later use
+    this.logger.debug(`User found for userId ${userId} in session, allowing`);
     request.user = user;
     return true;
   }
